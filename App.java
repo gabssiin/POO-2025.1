8fd0cef8fca775b2f089697ffa3232cc1b2c79cd
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2025-02-11 23:01:23 +0000

    1

--- App.java
+++ App.java
@@ -3,6 +3,27 @@ import java.util.Scanner;
 public class  App {
 
     public static void main (String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+        
+        String nome = ler.nextLine();
+        System.out.println("Digite seu Nome: " + nome); 
+        int idade = ler.nextInt();
+        System.out.println("Digite sua idade: " + idade );
+        double altura = ler.nextDouble();
+        System.out.println("Digite sua altura: " + altura );
+     
+        System.out.println(" ");
+        if (idade < 16){
+            System.out.println(nome + " Não poderá votar.");
+        }
+        else if (idade<18 || idade > 64 ){
+            System.out.println(nome + " Tem o voto facultativo");
+        }
+        else {
+            System.out.println(nome + "tem o voto obrigatório");
+        }
+
+/* 
         String name = "Gabs";
         int age = 20;
         double height = 1.30;
@@ -17,7 +38,9 @@ public class  App {
         else {
             System.out.println(name + " Tem o voto obrigatório");
         }
+*/
     }
+
 }
 
 
---
+++ Main.java
@@ -0,0 +1,28 @@
+    import java.util.Scanner;
+    public class Main {
+    
+    
+        public static void main (String[] args){
+            Scanner sc = new Scanner(System.in);
+            String nome = sc.nextLine();
+            System.out.println("Digite seu Nome: " + nome); 
+            int idade = sc.nextInt();
+            System.out.println("Digite sua idade: " + idade );
+            double altura = sc.nextDouble();
+            System.out.println("Digite sua altura: " + altura );
+         
+            System.out.println(" ");
+            if (idade < 16){
+                System.out.println(nome + " Não poderá votar.");
+            }
+            else if (idade<18 || idade > 64 ){
+                System.out.println(nome + " Tem o voto facultativo");
+            }
+            else {
+                System.out.println(nome + "tem o voto obrigatório");
+            }
+    
+    
+    
+    }
+}
