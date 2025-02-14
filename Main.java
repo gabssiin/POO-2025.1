233034df75406fa12d0abc865f3f8899f977dc69
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2025-02-14 22:52:54 +0000

    funções

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
-    import java.util.Scanner;
-    public class Main {
-    
-    
+import java.util.Scanner;
+
+public class Main {
+        
+    public static double calculaIMC(double altura, double peso){
+        return peso/(altura*altura);
+
+    }
         public static void main (String[] args){
-            /* 
+            
             Scanner sc = new Scanner(System.in);
+            /* 
             String nome = sc.nextLine();
             System.out.println("Digite seu Nome: " + nome); 
             int idade = sc.nextInt();
@@ -23,7 +28,7 @@
                 System.out.println(nome + "tem o voto obrigatório");
             }
              */ 
-
+/* 
             Scanner sc = new Scanner(System.in);
             System.out.println("Sistemas de idade, digite quantos anos você tem: ");
             int idade = sc.nextInt();
@@ -31,6 +36,18 @@
                 System.out.println("Feliz Aniversário");
             }
             sc.close();
-            
+ */ 
+
+ 
+System.out.println("Sistema para IMC");
+System.out.println("Digite seu peso: ");
+double peso = sc.nextDouble();
+System.out.println(" ");
+System.out.println("Digite sua altura: ");
+double altura = sc.nextDouble();
+double resultado = calculaIMC(altura, peso);
+System.out.println("Seu IMC é: "+ resultado);
+sc.close();
+
     }
 }
